3af41b7369000dda408e67757df7bfc9a55e0ede
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2025-11-05 10:39:42 +0500

    Add files via upload

---
+++ binaryTree/MorrisInorderTraversel.cpp
@@ -0,0 +1,5 @@
+inorder predecessor (IP):
+inorder sequence me node se pehly any waly node ko katyy hy 
+
+root node ka inorder predecessor hamesha left subtree ka ander sub se right most node hoga
+k
